73375b117f82c9ba1a6e84ab3625cd5fe03c5bff
Owen Sims <owen.sims@cyberlytic.com>
2018-07-26 14:47:23 +0100

    Add mission data

---
+++ utils/data/MissionData.py
@@ -0,0 +1,20 @@
+default_missions = [
+    {
+        "id": "DPS-MISSION",
+        "name": "Operation Thunderbolt",
+        "description": "Defend power station",
+        "criticalFunctions": [
+            "POWER-STATION-F1"
+        ],
+        "systems": [
+            "POWER-STATION"
+        ],
+        "units": [
+            "JV1",
+            "JV2",
+            "JV3",
+            "hostileSpecialForces",
+            "hacker"
+        ]
+    }
+]
