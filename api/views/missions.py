93d0470e36b4037042b574dddbb86e1ed3c08521
Owen Sims <owen.sims@cyberlytic.com>
2018-07-26 14:50:01 +0100

    Update the mission endpoints

--- api/views/missions.py
+++ api/views/missions.py
@@ -1,60 +1,56 @@
+import os
+import pymongo as pm
+
 from rest_framework.response import Response
-from rest_framework_mongoengine.generics import ListCreateAPIView, RetrieveDestroyAPIView, ListAPIView
+from rest_framework.renderers import JSONRenderer
+from rest_framework.generics import ListAPIView, ListCreateAPIView
+from drf_yasg.utils import swagger_auto_schema
 
-from api.models.missions import Missions
-from api.serializers.missions import MissionDataSerializer
+from api.models.missions import MissionsModel
 
 
-class MissionDetail(ListCreateAPIView, RetrieveDestroyAPIView):
-    """
-    Query the `missions` collection in Mongo.
-    """
-    serializer_class = MissionDataSerializer
+class MissionsView(ListCreateAPIView):
+    renderer_classes = (JSONRenderer,)
+    serializer_class = MissionsModel
 
-    def get_queryset(self):
-        """
-        Collect all documents (missions) from the `missions` collection in Mongo.
-        """
-        queryset = Missions.objects.all()
-        return queryset
+    def __init__(self):
+        self.missions_collection = pm.MongoClient(
+            host=os.environ.get('DB_HOSTNAME'),
+            port=int(os.environ.get('DB_PORT'))
+        )[os.environ.get('DB_NAME')]['missions']
 
+    @swagger_auto_schema(responses={200: "OK"})
+    def get(self, request, *args, **kwargs):
+        missions_list = list(self.missions_collection.find())
+        for mission in missions_list:
+            mission['_id'] = str(mission['_id'])
+        return Response(
+            data=missions_list,
+            status=200)
+
+    @swagger_auto_schema(responses={201: "Created"})
     def post(self, request, *args, **kwargs):
-        """
-        Store mission data in the `missions` collection in Mongo.
-        """
-        self.create(request, *args, **kwargs)
+        MissionsModel(
+            data=request.data).is_valid(
+            raise_exception=True)
+        self.missions_collection.insert(request.data)
         return Response(status=201)
 
-    def delete(self, request, *args, **kwargs):
-        """
-        Drop the `missions` Mongo collection.
-        """
-        self.queryset = Missions.drop_collection()
-        return Response(status=204)
 
+class MissionIdView(ListAPIView):
+    renderer_classes = (JSONRenderer,)
+    serializer_class = MissionsModel
 
-class MissionIdDetail(ListAPIView):
-    """
-    Query the `missions` collection in Mongo by mission ID, given by `missionID`.
-    """
-    serializer_class = MissionDataSerializer
+    def __init__(self):
+        self.missions_collection = pm.MongoClient(
+            host=os.environ.get('DB_HOSTNAME'),
+            port=int(os.environ.get('DB_PORT'))
+        )[os.environ.get('DB_NAME')]['missions']
 
-    def get_queryset(self):
-        mission_id = self.kwargs['missionId']
-        if mission_id is not None:
-            queryset = Missions.objects.filter(missionId__iexact=mission_id)
-        return queryset
-
-
-class MissionObjectiveDetail(ListAPIView):
-    """
-    Query the `missions` collection in Mongo by mission objective, given by `missionObjective`.
-    """
-    serializer_class = MissionDataSerializer
-
-    def get_queryset(self):
-        mission_objective = self.kwargs['missionObjective']
-
-        if mission_objective is not None:
-            queryset = Missions.objects.filter(missionObjective__iexact=mission_objective)
-        return queryset
+    @swagger_auto_schema(responses={200: "OK"})
+    def get(self, request, *args, **kwargs):
+        mission = list(self.missions_collection.find(
+            {"id": self.kwargs['missionId']}))
+        return Response(
+            data=mission,
+            status=200)
