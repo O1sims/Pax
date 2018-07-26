fbaa739c7eb5ba79af1e8b9ccc0b3d7365539e27
Owen Sims <owen.sims@cyberlytic.com>
2018-07-26 14:46:33 +0100

    Add unit data

---
+++ utils/data/UnitData.py
@@ -0,0 +1,69 @@
+default_units = [
+    {
+        "id": "JV1",
+        "name": "Cyber Platoon",
+        "affiliation": "FRIENDLY",
+        "offensiveCapability": 5,
+        "defensiveCapability": 3,
+        "description": "",
+        "numberOfPersonnel": 30,
+        "specialism": "CYBER",
+        "equipment": [
+            "laptop",
+            "tin foil"
+        ]
+    },
+    {
+        "id": "JV2",
+        "name": "Brigade Force",
+        "affiliation": "FRIENDLY",
+        "offensiveCapability": 2,
+        "defensiveCapability": 4,
+        "description": "",
+        "numberOfPersonnel": 12,
+        "specialism": "PHYSICAL",
+        "equipment": [
+            "gun"
+        ]
+    },
+    {
+        "id": "JV3",
+        "name": "Infantry",
+        "affiliation": "FRIENDLY",
+        "offensiveCapability": 4,
+        "defensiveCapability": 4,
+        "description": "",
+        "numberOfPersonnel": 10,
+        "specialism": "PHYSICAL",
+        "equipment": [
+            "machine gun"
+        ]
+    },
+    {
+        "id": "hostileSpecialForces",
+        "name": "Hostile Special Forces",
+        "affiliation": "HOSTILE",
+        "offensiveCapability": 2,
+        "defensiveCapability": 2,
+        "description": "",
+        "numberOfPersonnel": 4,
+        "specialism": "PHYSICAL",
+        "equipment": [
+            "gun"
+        ]
+    },
+    {
+        "id": "hacker",
+        "name": "Hacker",
+        "affiliation": "HOSTILE",
+        "offensiveCapability": 3,
+        "defensiveCapability": 3,
+        "description": "",
+        "numberOfPersonnel": 1,
+        "specialism": "CYBER",
+        "equipment": [
+            "laptop",
+            "usb stick"
+        ]
+    }
+]
